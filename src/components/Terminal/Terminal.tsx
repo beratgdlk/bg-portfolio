12838e6edc64c9269879ccb42c62b9f4bf32565c
Berat Güdelek <beratgdlk@gmail.com>
2025-02-10 23:20:08 +0300

    1

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -9,7 +9,7 @@ const profileData: ProfileData = {
     telephoneNum: "392 86 88 006",
     email: "davide_simone1996@proton.me",
     githubLink: "https://github.com/beratgdlk",
-    behanceLink: "https://www.behance.net/davidefemale",
+    mediumLink: "https://www.behance.net/davidefemale",
     linkedinPage: "https://www.linkedin.com/in/davide-simone-a2823619a/"
   }
 };
@@ -32,8 +32,8 @@ const Terminal: React.FC = () => {
           <p className="comment">// you can also see it on my GitHub page</p>
           <p className="const">const githubLink = "{profileData.contact.githubLink}";</p>
           
-          <p className="comment">// you can also see my projects on Behance</p>
-          <p className="const">const behanceLink = "{profileData.contact.behanceLink}";</p>
+          <p className="comment">// you can see my blog on Medium</p>
+          <p className="const">const mediumLink = "{profileData.contact.mediumLink}";</p>
           
           <p className="comment">// you can check my LinkedIn page</p>
           <p className="const">const linkedinPage = "{profileData.contact.linkedinPage}";</p>
