c48cb5d520b52acb59becfa01bab1c6403c7c473
Berat Güdelek <beratgdlk@gmail.com>
2025-02-11 01:52:10 +0300

    update

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -6,7 +6,7 @@ const profileData: ProfileData = {
   name: "Berat Gudelek",
   title: "Front-end developer",
   contact: {
-    telephoneNum: "538 059 15 16",
+    telephoneNum: "+90 538 059 15 16",
     email: "beratgdlk@gmail.com",
     githubLink: "https://github.com/beratgdlk",
     mediumLink: "https://medium.com/@beratgdlk",
