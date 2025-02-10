a3558e71c548641993b635fd1c05f04985ff06df
Berat Güdelek <beratgdlk@gmail.com>
2025-02-11 01:51:44 +0300

    navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,8 +4,15 @@ import './Navbar.scss';
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
-      <div className="navbar__logo">berat-gudelek</div>
-      <div className="navbar__links">
+      <div className="navbar__left">
+        <span>berat-gudelek</span>
+      </div>
+      
+      <div className="navbar__center">
+        <img src="/assets/images/bg-logo.png" alt="logo" className="navbar__logo" />
+      </div>
+      
+      <div className="navbar__right">
         <a href="#" className="active">_hello</a>
         <a href="#">_about-me</a>
         <a href="#">_projects</a>
