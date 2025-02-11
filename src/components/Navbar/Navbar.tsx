0d0c4835306ecdb66a8a557ba1a9fbe188a280b4
Berat Güdelek <beratgdlk@gmail.com>
2025-02-11 03:17:34 +0300

    responsive arrangements

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.scss';
 
 const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar__left">
@@ -12,7 +18,13 @@ const Navbar: React.FC = () => {
         <img src="/assets/images/bg-logo.png" alt="logo" className="navbar__logo" />
       </div>
       
-      <div className="navbar__right">
+      <button className="navbar__hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      
+      <div className={`navbar__right ${isMenuOpen ? 'active' : ''}`}>
         <a href="#" className="active">_hello</a>
         <a href="#">_about-me</a>
         <a href="#">_projects</a>
